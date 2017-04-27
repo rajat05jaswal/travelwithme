3a5fdab4e5b14f677a98b45be925f43e774b40fd
Rajat Jaswal <rajat05jaswal@gmail.com>
2017-04-27 20:25:26 +0530

    Unwanted Node Module

--- src/containers/SignIn.js
+++ src/containers/SignIn.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
-import {Text,View,StyleSheet,Image} from 'react-native';
-import { Container, Content, Form, Item, Input,Button,Header
-  ,Left,Icon,Body,Title,Right,Label,Toast,Spinner } from 'native-base';
+import {Text,View,StyleSheet,Image,TextInput,TouchableOpacity} from 'react-native';
+// import { Container, Content, Form, Item, Input,Button,Header
+//   ,Left,Icon,Body,Title,Right,Label,Toast,Spinner } from 'native-base';
 import firebase from 'firebase';
 export default class SignIn extends Component{
   constructor(props){
@@ -19,6 +19,7 @@ export default class SignIn extends Component{
     firebase.auth().signInWithEmailAndPassword(email,password)
     .then(()=>{
       this.setState({loading:false});
+      alert("SIGNED IN SUCCESFULLY");
     })
     .catch(()=>{
       this.setState({loading:false});
@@ -37,67 +38,72 @@ export default class SignIn extends Component{
   }
   render() {
         return (
-              <Container>
-                  <Header>
-                      <Left>
-                          <Button transparent>
-                              <Icon name='menu' />
-                          </Button>
-                      </Left>
-                      <Body>
-                          <Title>Sign In</Title>
-                      </Body>
-                      <Right />
-                  </Header>
-                  <Content padder>
-                      <Form>
-                          <Item floatingLabel>
-                              <Label>Email</Label>
-                              <Input
-                              onChangeText={(email) => this.setState({email})}
-                              value={this.state.email}
-                              />
-                          </Item>
-                          <Item floatingLabel last>
-                              <Label>Password</Label>
-                              <Input
-                              onChangeText={(password) => this.setState({password})}
-                              value={this.state.password}
-                              secureTextEntry={true}
-                              />
-                          </Item>
-                          <View style={styles.signInSignOutWrapper}>
-                              {this.state.loading?
-                                <Spinner
-                                style={{marginLeft:20}}
-                                />
-                                :
-                                <Button rounded large onPress={this.signInUser.bind(this)}>
-                                  <Text style={styles.text}>Sign In </Text>
-                                </Button>
-                              }
-                              <Button rounded large>
-                                <Text style={styles.text}>Sign Up </Text>
-                              </Button>
-                          </View>
-                      </Form>
-
-                  </Content>
-              </Container>
+              <View style={styles.wrapper}>
+                <Text style={{fontSize:20,textAlign:'center',color:'black'}}> Sign In To Continue </Text>
+                <View >
+                  <TextInput
+                    placeholder="Email"
+                      style={[styles.textInput,styles.label]}
+                      onChangeText={(email) => {
+                        this.setState({email:email});
+                      }}
+                      value={this.state.email}
+                  />
+                </View>
+                <View>
+                  <TextInput
+                    placeholder="Password"
+                      style={[styles.textInput,styles.label]}
+                      onChangeText={(password) => {
+                        this.setState({password:password});
+                      }}
+                      value={this.state.password}
+                  />
+                </View>
+                <View elevation={10} style={styles.signInButton}>
+                  <TouchableOpacity onPress={()=>{this.signInUser.bind(this)}>
+                  <Text style={{fontSize:30,color:'white',
+                  textAlign:'center',}}>SIGN IN</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
         );
     }
 }
+
 const styles = StyleSheet.create({
-  signInSignOutWrapper:{
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginTop:20,
-  },
-  text:{
-    color:'white',
-  },
+  // signInSignOutWrapper:{
+  //   alignItems:'center',
+  //   justifyContent:'center',
+  //   marginTop:20,
+  // },
+  // text:{
+  //   color:'white',
+  // },
   backgroundImage:{
     flex:1,
     resizeMode:'cover',
+  },
+  passwordLabel:{
+    marginTop:30,
+  },
+  textInput:{
+    textAlign:'center',
+  },
+  wrapper:{
+    padding:50,
+    alignItems:'center',
+    flex:1,
+  },
+  label:{
+    marginTop:30,
+    width:280,
+  },
+  signInButton:{
+    marginTop:40,
+    padding:20,
+    width:200,
+    borderRadius:50,
+    backgroundColor:'#feac00',
   }
 });
