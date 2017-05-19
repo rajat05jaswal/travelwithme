435d1e8d4f9686c7e26c554d1904369c410befd2
srishti saboo <srishti saboo>
2017-05-19 19:55:17 -0400

    Rewritten Index.js Page for testing Profile Page

--- src/containers/index.js
+++ src/containers/index.js
@@ -4,15 +4,15 @@ import {Provider} from 'react-redux';
 import {createStore,applyMiddleware} from 'redux';
 import Reducer from '../reducers/index';
 import SignIn from './SignIn';
-import TestFacebook from './TestFacebook';
-// import MainNavigator from './MainNavigator';
+import TestFacebook from './TestFacebook.js';
+import Profile from '../components/Profile';
 
 const store = createStore(Reducer);
 export default class Main extends Component{
   render(){
     return(
       <Provider store={store}>
-        <SignIn/>
+        <Profile />
       </Provider>
     )
   }
